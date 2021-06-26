77f706092dbdbe32393b203a8a585d766c2ed3e9
rahulnadar2209 <rahulnadar2209@gmail.com>
2021-06-26 17:20:09 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './button/button.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, ButtonComponent, TasksComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, ButtonComponent, TasksComponent, TaskItemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
